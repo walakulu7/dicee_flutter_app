1228df874b9083f1b59fd549a0425724ac22df54
Janaka Walakuluarachchi <walakulu@gmail.com>
2022-10-08 21:57:52 +0530

    Dicee created

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: Image(
+            image: AssetImage('images/dice1.png'),
+          ),
+        )
+      ],
+    );
   }
 }
+
+
