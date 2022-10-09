13ba05e1401ad992d22b30f3cbaf07ac1042fd5e
Janaka Walakuluarachchi <walakulu@gmail.com>
2022-10-09 23:15:46 +0530

    Button widget created

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                print('Left button got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                print('Right button got pressed');
+              },
+                child: Image.asset('images/dice1.png'),
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
